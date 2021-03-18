f15c2b164e91b79bc3df2bc244481979dd4ce737
Nicolas P <n.perezbus@gmail.com>
2021-03-18 18:44:36 -0300

    Adding top button

--- script.js
+++ script.js
@@ -30,3 +30,37 @@ if (theme == null) {
 } else {
 	setTheme(theme);
 }
+
+let buttons = document.getElementsByClassName('navigation-link');
+[].forEach.call(buttons, (button) => {
+	button.addEventListener('click', (e) => {
+		e.preventDefault();
+		let target = e.currentTarget.getAttribute('href');
+
+		document.querySelector(target).scrollIntoView({
+			behavior: 'smooth',
+		});
+	});
+});
+
+// Button to Top:
+//Get the button:
+mybutton = document.getElementById('toTopButton');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = 'block';
+	} else {
+		mybutton.style.display = 'none';
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	window.scroll({ top: 0, behavior: 'smooth' });
+}
